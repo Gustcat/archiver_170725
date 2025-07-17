dbde7bb0c54eccc8efc3a3ff7266a1eac8bf9647
Екатерина Густова <antinpanic@gmail.com>
2025-07-17 16:44:22 +0300

    Add base for project

---
+++ cmd/http-server/main.go
@@ -0,0 +1,64 @@
+package main
+
+import (
+	"context"
+	"github.com/Gustcat/archiver_170725/internal/config"
+	"github.com/Gustcat/archiver_170725/internal/logger"
+	"github.com/gin-gonic/gin"
+	"github.com/joho/godotenv"
+	"log/slog"
+	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+)
+
+func main() {
+	log := logger.SetupLogger(slog.LevelDebug)
+
+	ctx := context.Background()
+
+	err := godotenv.Load(".env")
+	if err != nil {
+		log.Warn("doesn't load env file: %s", slog.String("error", err.Error()))
+	}
+
+	conf, err := config.New()
+	if err != nil {
+		log.Error("doesn't set config: %s", slog.String("error", err.Error()))
+		os.Exit(1)
+	}
+
+	log.Debug("Try to setup router")
+	router := gin.New()
+
+	router.Use(gin.Recovery())
+
+	srv := &http.Server{
+		Addr:         conf.Address,
+		Handler:      router,
+		ReadTimeout:  conf.Timeout,
+		WriteTimeout: conf.Timeout,
+		IdleTimeout:  conf.IdleTimeout,
+	}
+
+	quit := make(chan os.Signal, 1)
+	signal.Notify(quit, syscall.SIGINT, syscall.SIGTERM)
+
+	go func() {
+		if err := srv.ListenAndServe(); err != nil {
+			log.Error("failed to start http server", slog.String("error", err.Error()))
+		}
+	}()
+
+	log.Info("Server started", slog.String("address", conf.Address))
+
+	<-quit
+
+	if err := srv.Shutdown(ctx); err != nil {
+		log.Error("failed to shutdown http server", slog.String("error", err.Error()))
+		return
+	}
+
+	log.Info("Server stopped", slog.String("address", conf.Address))
+}
---
+++ internal/config/config.go
@@ -0,0 +1,32 @@
+package config
+
+import (
+	"fmt"
+	"time"
+
+	"github.com/caarlos0/env/v10"
+)
+
+type HTTPServer struct {
+	Host        string `env:"HTTP_HOST" envDefault:"localhost"`
+	Port        string `env:"HTTP_PORT" envDefault:"8081"`
+	Address     string
+	Timeout     time.Duration `env:"HTTP_TIMEOUT" envDefault:"5s"`
+	IdleTimeout time.Duration `env:"HTTP_IDLE_TIMEOUT" envDefault:"60s"`
+	User        string        `env:"HTTP_USER" envDefault:"user"`
+	Password    string        `env:"HTTP_PASSWORD" envDefault:"password"`
+}
+
+func New() (*HTTPServer, error) {
+	cfg := &HTTPServer{}
+	if err := env.Parse(cfg); err != nil {
+		return nil, fmt.Errorf("loading config from env is failed: %w", err)
+	}
+	buildHTTPAddress(cfg)
+
+	return cfg, nil
+}
+
+func buildHTTPAddress(httpserver *HTTPServer) {
+	httpserver.Address = fmt.Sprintf("%s:%s", httpserver.Host, httpserver.Port)
+}
